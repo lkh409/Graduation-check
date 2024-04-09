e6a69108b30c98e97c9673193725cf3368f90463
suin3559 <suin3559@gmail.com>
2024-04-09 22:58:11 +0900

    Create Check_page Components

--- src/App3.js
+++ src/App3.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-//import SidebarContainer3 from './components/SidebarContainer';
-import MainContentContainer3 from './components/MainContentContainer';
 import './App3.css'
+import Check_page from './Check_page3';
+import './components/Check_page.css'
 import Navbar from './components/Navbar2';
 import Footer from './components/Footer';
 
@@ -11,17 +11,7 @@ function App3() {
     <div className="app-container">
       <Router>
       <Navbar />
-      
-      <h2 className='success-check'>졸업요건 검사 완료!</h2>
-      <p>그래프를 클릭하면 자세한 정보와 수정이 가능합니다.</p>
-      <div className="content-container">
-        {/*<SidebarContainer3 />*/}
-        <MainContentContainer3 title="기초교양"/>
-        <MainContentContainer3 title="일반교양"/>
-        <MainContentContainer3 title="자유교양"/>
-        <MainContentContainer3 title="전공필수"/>
-        <MainContentContainer3 title="전공선택"/>
-      </div>
+      <Check_page />
       <Footer/>
       </Router>
     </div>
---
+++ src/Check_page3.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import MainContentContainer3 from './components/MainContentContainer';
+import './App3.css'
+import './components/Check_page.css'
+
+function Check_page() {
+  return (
+    <>
+      <h2 className='success-check'>졸업요건 검사 완료!</h2>
+      <p>그래프를 클릭하면 자세한 정보와 수정이 가능합니다.</p>
+      <div className="content-container">
+        {/*<SidebarContainer3 />*/}
+        <MainContentContainer3 title="기초교양"/>
+        <MainContentContainer3 title="일반교양"/>
+        <MainContentContainer3 title="자유교양"/>
+        <MainContentContainer3 title="전공필수"/>
+        <MainContentContainer3 title="전공선택"/>
+      </div>
+    </>
+  );
+}
+
+export default Check_page;
