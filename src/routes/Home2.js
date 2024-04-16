8d301af65f47c74c382f8947dcbd4a374dd08e82
suin3559 <suin3559@gmail.com>
2024-04-16 18:52:57 +0900

    Combine the code related to the Home page of develop1 with my code, adjust the screen after generating Home.css, and modify the Recommend button on the Check page

--- src/routes/Home2.js
+++ src/routes/Home2.js
@@ -1,16 +1,34 @@
+// Home2.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Layout, Button } from 'antd';
+import { Routes, Route } from 'react-router-dom'; // BrowserRouter를 import 하지 않습니다.
+import '../styles/App3.css';
+import '../components/MenuItems';
+import { Link } from "react-router-dom";
 
 function Home() {
+  const { Content } = Layout;
+
   return (
     <>
-      <h1 className="home-title">메인 홈화면</h1>
-      <div className="horizontal-line"></div>
-      <div className='home-content'>
-      <button>
-        <Link to="/check" className='chk-btn'>검사하기</Link>
-        </button>
-    </div>
+      <Layout className='app-layout'>
+        <Content className='app-content'>
+          <div className="home-image-container">
+            <div className="home-big-title">Graduation, the moment</div>
+            <div className="home-small-title">Never give up, your dreams will come true</div>
+            <img
+              src="https://cdn.pixabay.com/photo/2019/02/07/11/22/learn-3980941_1280.jpg"
+              alt="사진"
+              style={{ margin: 'auto', display: 'block' }}
+            />
+            <div className="home-button-container">
+              <Button className="home-primary-button">
+              <Link to={"/check"}>검사하기</Link>
+              </Button>
+            </div>
+          </div>
+        </Content>
+      </Layout>
     </>
   );
 }
